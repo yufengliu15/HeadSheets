c9a7f13d4a5ce575a55a9c92c6bdeaa11dd826ad
Yufeng Liu <yufeng.liu15@gmail.com>
2023-10-25 19:03:33 -0400

    multiplatform

---
+++ HeadSheets/HeadSheets/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HeadSheets
+//
+//  Created by Yufeng Liu on 2023-10-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HeadSheets/HeadSheets/HeadSheetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeadSheetsApp.swift
+//  HeadSheets
+//
+//  Created by Yufeng Liu on 2023-10-25.
+//
+
+import SwiftUI
+
+@main
+struct HeadSheetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
